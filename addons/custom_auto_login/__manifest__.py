d1ae72957832056e4b53c5f6a78eb4aa683d9cd3
toby <270634533@qq.com>
2024-05-24 16:22:02 +0800

    自定义登录，登录成功之后重定向到首页

---
+++ addons/custom_auto_login/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Auto Login',
+    'description': 'Auto login using session_id',
+    'version': '1.0',
+    'category': 'Custom',
+    'depends': ['web'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
