6b82a0590d8187dac3a0c8e3726637ede472ff17
toby <270634533@qq.com>
2024-05-27 17:16:33 +0800

    自定义登出模块

--- addons/custom_auto_login/__manifest__.py
+++ addons/custom_auto_login/__manifest__.py
@@ -8,7 +8,6 @@
     'auto_install': False,
     'license': 'LGPL-3',
     'application': True,        #是否显示在apps列表里面
-    'images': ['static/description/login-logo.png'],  # 添加自定义图标
     'summary':'A module for auto login using session_id',#简短说明
     'description':"""
         ========================
@@ -16,5 +15,5 @@
     """,                                    #详细说明
     'maintainer':'南京联成科技发展股份有限公司', #模块的维护者信息
     'author':'南京联成科技发展股份有限公司',     #模块的作者信息
-    'website':'www.lsmsp.cn',               #作者或模块相关网站
+    'website':'http://www.lsmsp.cn',               #作者或模块相关网站
 }
