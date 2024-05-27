9d345e3e04262a83abf06f9c3d023215d6d834cc
toby <270634533@qq.com>
2024-05-27 10:44:41 +0800

    完善自定义登录模块

--- addons/custom_auto_login/__manifest__.py
+++ addons/custom_auto_login/__manifest__.py
@@ -1,6 +1,5 @@
 {
     'name': 'Custom Auto Login',
-    'description': 'Auto login using session_id',
     'version': '1.0',
     'category': 'Custom',
     'depends': ['web'],
@@ -8,4 +7,14 @@
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
+    'application': True,        #是否显示在apps列表里面
+    'images': ['static/description/login-logo.png'],  # 添加自定义图标
+    'summary':'A module for auto login using session_id',#简短说明
+    'description':"""
+        ========================
+        This module allows users to automatically log in using a session_id
+    """,                                    #详细说明
+    'maintainer':'南京联成科技发展股份有限公司', #模块的维护者信息
+    'author':'南京联成科技发展股份有限公司',     #模块的作者信息
+    'website':'www.lsmsp.cn',               #作者或模块相关网站
 }
