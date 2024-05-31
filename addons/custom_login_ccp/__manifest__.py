9c79a002269df154cf4e29014cfa7cf054d5b6af
toby <270634533@qq.com>
2024-05-31 15:06:47 +0800

    session失效的时候强制跳转到ccp登录界面，避免跳转到odoo登录界面

--- addons/custom_login_ccp/__manifest__.py
+++ addons/custom_login_ccp/__manifest__.py
@@ -8,4 +8,7 @@
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
+    'maintainer': '南京联成科技发展股份有限公司',  # 模块的维护者信息
+    'author': '南京联成科技发展股份有限公司',  # 模块的作者信息
+    'website': 'http://www.lsmsp.cn',  # 作者或模块相关网站
 }
